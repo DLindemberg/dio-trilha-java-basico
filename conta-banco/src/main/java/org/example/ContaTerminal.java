41c184ad6646f8aa02869da7612020b663bc604c
DLindemberg <davitacidelli@hotmail.com>
2024-10-29 01:13:00 -0300

    Desafio concluído.

--- conta-banco/src/main/java/org/example/ContaTerminal.java
+++ conta-banco/src/main/java/org/example/ContaTerminal.java
@@ -1,11 +1,34 @@
 package org.example;
 
-public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+public class ContaTerminal {
+
+    public static void criarContaBanco() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        String saldoInput = scanner.nextLine();
+        saldoInput = saldoInput.replace(",", ".");
+        double saldo = Double.parseDouble(saldoInput);
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+    }
+
+    public static void main(String[] args) {
+        criarContaBanco();
     }
 }
